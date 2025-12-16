033022e2144dd739338341e57421047ffacd5cca
Mohamed ashraf <muhamed77700m@gmail.com>
2025-12-16 05:40:33 +0200

    gsap training

---
+++ pages/05a_back-easing/script.js
@@ -0,0 +1,29 @@
+import gsap from "gsap";
+const tabs = document.querySelectorAll(".tab");
+const tabRow = document.querySelector(".tab-row");
+const indicator = document.querySelector(".indicator");
+
+const updateIndicator = (target) => {
+  const rowBounds = tabRow.getBoundingClientRect();
+  const targetBounds = target.getBoundingClientRect();
+
+  const offset = Number(targetBounds.left) - rowBounds.left;
+  const width = targetBounds.width;
+  console.log(targetBounds.width);
+  gsap.to(indicator, {
+    x: offset,
+    width,
+    duration: 0.4,
+    ease: "back.out(1)",
+  });
+};
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tab.classList.add("active");
+    updateIndicator(tab);
+  });
+});
+
+updateIndicator(document.querySelector(".tab.active"));
